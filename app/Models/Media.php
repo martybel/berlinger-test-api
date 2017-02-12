aad845edfcbd8dbd0d6d4bc7dc05049854462ad1
Peter Eussen <peter.eussen@gmail.com>
2017-02-12 11:55:54 +0100

    Make sure we use the facade here as well

--- app/Models/Media.php
+++ app/Models/Media.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Facades\File;
 
 class Media extends Model
 {
